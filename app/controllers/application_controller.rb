2997a1076f32c4d0e35add0f3751e4298df33b3d
Ross <rossgarfield@gmail.com>
2022-06-30 11:58:13 -0500

    commit tree 9dc44d9a042afe2d32b370ebfa656bfac57b5119

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    private
+
+    def render_not_found_response(exeption)
+        render json: { error: "#{exeption.model} not found" }, status: :not_found
+    end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,28 @@
 class ItemsController < ApplicationController
-
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
     items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
 end
